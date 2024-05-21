068f3e281ab8de4759ae550e4c8efcad71e08a60
Aiden <amb@aspy.dev>
2024-05-20 21:42:20 -0400

    Very basic PoC (basic login implementation)

---
+++ .github/readme.md
@@ -0,0 +1,7 @@
+# Dractron
+
+Electron app for iDRAC's virtual console.
+
+Only works for iDrac 8+, as these versions support HTML5 based KVMs
+
+Untested on iDrac 9. I do not have access to an iDrac 9 machine.
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from "electron";
+
+
+const url = "https://192.168.1.215/"
+
+import { user, pass} from "../indevconfig.json"
+
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+
+const res = await fetch(url+"/data/login", {
+    method: "POST",
+    body: `user=${user}&password=${pass}`,
+})
+
+const XmlResponse = await res.text();
+
+
+app.once("ready", ()=>{
+    const window = new BrowserWindow();
+    void window.loadURL(url+/(?<=<forwardUrl>)(.*)(?=<\/forwardUrl>)/.exec(XmlResponse)![0])
+})
+
+app.on('certificate-error',(event, __webContents, __url, __error, certificate, callback)=>{
+    event.preventDefault();
+    if(certificate.issuer.organizations.at(0)==="Dell Inc."){
+        // Easily spoofed, but if you are using this you should know what you are doing...
+        // FIXME - ADD METHOD TO OVERRIDE
+        callback(true);
+        return;
+    }
+    callback(false);
+})
