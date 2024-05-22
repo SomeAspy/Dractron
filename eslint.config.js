50731eeacf436ed048bd88938141a5fdc324de3d
Aiden <amb@aspy.dev>
2024-05-22 19:40:30 -0400

    Fix bad relative links reimplement logout

--- eslint.config.js
+++ eslint.config.js
@@ -2,24 +2,33 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import stylisticJs from "@stylistic/eslint-plugin-js";
 
 export default tseslint.config(
-  eslint.configs.recommended,
-  {ignores:["dist/"]},
-  ...tseslint.configs.strictTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
-  {
+
+    eslint.configs.recommended,
+    {ignores:["dist/"]},
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
+    {
+        plugins: {
+            "@stylistic/js":stylisticJs
+        },
     
-    languageOptions: {
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+        languageOptions: {
+            parserOptions: {
+                project: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+        rules:{
+            "@typescript-eslint/no-non-null-assertion": "off",
+            "@typescript-eslint/restrict-template-expressions": "warn",
+            "@typescript-eslint/prefer-for-of": "off",
+            "@typescript-eslint/no-misused-promises": "off",
+            '@stylistic/js/semi': "error",
+            '@stylistic/js/indent': "error",
+
+        }
     },
-    rules:{
-      "@typescript-eslint/no-non-null-assertion": "off",
-      "@typescript-eslint/restrict-template-expressions": "warn",
-      "@typescript-eslint/prefer-for-of": "off"
-    }
-  },
 );
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,34 @@
 import { app, BrowserWindow, session } from "electron";
 
-import untypedJson from "../indevconfig.json" with {type: "json"}
+import untypedJson from "../indevconfig.json" with {type: "json"};
 
-const { pass, user, url } = untypedJson as {pass:string; user:string; url:string;}
+const { pass, user } = untypedJson as {pass:string; user:string; url:string;};
+let { url } = untypedJson as {pass:string; user:string; url:string;};
 
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+
+if(url.endsWith("/")){
+    url = url.slice(0,-1); // Dell's getRPViewerCurrentScriptPath() function will return garbage if there is a slash at the end
+}
 
 const res = await fetch(url+"/data/login", {
     method: "POST",
     body: `user=${user}&password=${pass}`,
-})
+});
 
 const loginCookie = res.headers.getSetCookie();
 const XmlResponse = await res.text();
-const authLink = /(?<=<forwardUrl>)(.*)(?=<\/forwardUrl>)/.exec(XmlResponse)
+const authLink = /(?<=<forwardUrl>)(.*)(?=<\/forwardUrl>)/.exec(XmlResponse);
 
-
-
-console.log(authLink)
+console.log(authLink);
 
 if(!authLink){
     console.error("FATAL: Could not get authentication tokens. You may have entered the login incorrectly or have max sessions opened.");
     process.exit();
 }
 
-const token1 = /(?<=ST1=)(.*)(?=,ST2)/.exec(authLink[0])
-const token2 = /(?<=ST2=)(.*)/.exec(authLink[0])
+const token1 = /(?<=ST1=)(.*)(?=,ST2)/.exec(authLink[0]);
+const token2 = /(?<=ST2=)(.*)/.exec(authLink[0]);
 
 if(!loginCookie[0]){
     console.error("FATAL: Could not extract login cookie!");
@@ -41,15 +44,14 @@ if(!token1 || !token2){
 app.once("ready", ()=>{
     
     // https://stackoverflow.com/a/64954227/15324411
-    session.defaultSession.webRequest.onBeforeSendHeaders({urls: [`${url}*`]}, (details, callback)=>{
-    details.requestHeaders["Cookie"] = loginCookie[0]!
-    callback({requestHeaders: details.requestHeaders})
+        session.defaultSession.webRequest.onBeforeSendHeaders({urls: [`${url}*`]}, (details, callback)=>{
+        details.requestHeaders["Cookie"] = loginCookie[0]!
+        callback({requestHeaders: details.requestHeaders})
     })
 
     const window = new BrowserWindow();
     window.webContents.openDevTools()
     void window.loadURL(`${url}/${authLink[0]}`)
-    //void window.webContents.executeJavaScript(`document.location="login.html${authLink[0]}"`)
 
     window.on("close", ()=>{
         // try to logout, so the session isn't left hanging
@@ -60,22 +62,26 @@ app.once("ready", ()=>{
 })
 */
 
-app.once("ready", ()=>{
+app.once("ready", async ()=>{
     
     // https://stackoverflow.com/a/64954227/15324411
-    session.defaultSession.webRequest.onBeforeSendHeaders({urls: [`${url}*`]}, (details, callback)=>{
-    details.requestHeaders["Cookie"] = loginCookie[0]!
-    callback({requestHeaders: details.requestHeaders})
-    })
+    session.defaultSession.webRequest.onBeforeSendHeaders({urls: [`${url}/*`]}, (details, callback)=>{
+        details.requestHeaders["Cookie"] = loginCookie[0]!;
+        callback({requestHeaders: details.requestHeaders});
+    });
 
     const window = new BrowserWindow();
-    window.webContents.openDevTools()
-    void window.loadURL(`${url}/${authLink[0]}`)
-    void window.loadURL(`${url}/virtualconsolehtml5.html?ipAddr=192.168.1.215&kvmPort=5900&vmPriv=true&lang=en&aimSession=99&ST2=${token2[0]}&TokenName=ST1&TokenKey=${token1[0]}`)
-    //void window.webContents.executeJavaScript(`document.location="login.html${authLink[0]}"`)
+    window.webContents.openDevTools();
+    await window.loadURL(`${url}/virtualconsolehtml5.html?ipAddr=192.168.1.215&kvmPort=5900&vmPriv=true&lang=en&aimSession=99&ST2=${token2[0]}&TokenName=ST1&TokenKey=${token1[0]}`)
+});
 
-
-})
+app.once("quit", ()=>{
+    const headers = new Headers;
+    headers.set("Cookie",loginCookie[0]!);
+    void fetch(`${url}/data/logout`, {
+        headers
+    });
+});
 
 app.on('certificate-error',(event, __webContents, __url, __error, certificate, callback)=>{
     event.preventDefault();
@@ -86,4 +92,4 @@ app.on('certificate-error',(event, __webContents, __url, __error, certificate, c
         return;
     }
     callback(false);
-})
+});
